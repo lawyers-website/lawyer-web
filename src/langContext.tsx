0f854ff8c9fd6c27815fa037618183f4de0143b2
Anukoola abhiram <anukoolaabhiram@Anukoolas-MacBook-Air.local>
2022-11-25 19:59:15 +0530

    removed unnecessary code

--- src/langContext.tsx
+++ src/langContext.tsx
@@ -1,10 +1,17 @@
-import { createContext, useContext, useState, useEffect, Dispatch, SetStateAction } from "react";
+import { createContext, useState, Dispatch, SetStateAction } from "react";
 
-export const language = createContext<{ lang: string; setLang: Dispatch<SetStateAction<string>>  }| null>(null);
+export const language = createContext<{
+  lang: string;
+  setLang: Dispatch<SetStateAction<string>>;
+} | null>(null);
 
-export default function LangContext({ children }:{children:React.ReactNode}) {
-  const [lang,setLang] = useState("en");
+export default function LangContext({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const [lang, setLang] = useState("en");
   return (
-    <language.Provider value={{lang, setLang}}>{children}</language.Provider>
+    <language.Provider value={{ lang, setLang }}>{children}</language.Provider>
   );
 }
